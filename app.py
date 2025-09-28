050d5d781d308b0411d601ca2055620b4c4d07eb
Jack <142954634+turbojack03@users.noreply.github.com>
2025-09-27 17:39:21 -0700

    added adminpanel, and adminsettings page. added new adminstyles.css for them.
    added adminpanel, and adminsettings page. added new adminstyles.css for them.

--- app.py
+++ app.py
@@ -113,6 +113,16 @@ def login():
 
     return render_template('login.html')
 
+@app.route("/adminpanel")
+def adminpanel():
+    return render_template("adminpanel.html")
+
+
+@app.route("/adminsettings")
+def adminsettings():
+    return render_template("adminsettings.html")
+
+
 @app.route('/logout')
 @login_required
 def logout():
